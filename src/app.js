9927532c341c81e210cd644b5ab6d12d09900d81
Syed_Waseem <syedarw4794@gmail.com>
2024-01-31 18:37:38 +0530

    fix bugs

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { pageNotFound, errorMiddleware } from "./middlewares/error404.middleware
 
 const app = express();
 
-app.use(cors())
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended:true, limit: "16kb" }))
