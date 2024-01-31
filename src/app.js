32db6b3cd27fd733b501c2d1901ba61dea5f1777
Syed_Waseem <syedarw4794@gmail.com>
2024-01-31 18:46:51 +0530

    fixed some bugs

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
-import { pageNotFound, errorMiddleware } from "./middlewares/error404.middleware.js"
 
 const app = express();
 
-
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended:true, limit: "16kb" }))
